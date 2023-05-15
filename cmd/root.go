994b56a273028fa969ba935c3d187db716c552aa
Ryu Tanabe <bellx2@gmail.com>
2023-05-15 13:20:04 +0900

    バージョン更新

--- cmd/root.go
+++ cmd/root.go
@@ -25,7 +25,7 @@ func Execute() {
 
 func init() {
 	rootCmd.CompletionOptions.HiddenDefaultCmd = true
-	rootCmd.Version="1.2.0"
+	rootCmd.Version="1.2.1"
 	rootCmd.PersistentFlags().StringP("port", "p", "auto", "Serial Port Name")
 	rootCmd.PersistentFlags().Bool("debug", false, "Show Debug Message")
 }
