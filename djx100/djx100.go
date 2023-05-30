dd8614a483323e787a3f805f32b3890b0f4ae0f4
Ryu Tanabe <bellx2@gmail.com>
2023-05-30 20:33:59 +0900

    モードにTypeXとICDUを追加

--- djx100/djx100.go
+++ djx100/djx100.go
@@ -52,7 +52,7 @@ func(d *ChData) SetName(name string){
 	d.Name = n
 }
 
-var ChMode = []string{"FM", "NFM", "AM", "NAM", "T98", "T102_B54", "DMR", "T61_typ1", "T61_typ2","T61_typ3","T61_typ4","", "", "dPMR","DSTAR","C4FM","AIS","ACARS","POCSAG","12KIF_W","12KIF_N" }
+var ChMode = []string{"FM", "NFM", "AM", "NAM", "T98", "T102_B54", "DMR", "T61_typ1", "T61_typ2","T61_typ3","T61_typ4","T61_typx", "ICDU", "dPMR","DSTAR","C4FM","AIS","ACARS","POCSAG","12KIF_W","12KIF_N" }
 
 func ChMode2Num(mode string) (int){
 	for i, v := range ChMode {
