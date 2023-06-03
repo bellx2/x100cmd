4de84cca84b4c21d4a23227008354dd589a9defc
Ryu Tanabe <bellx2@gmail.com>
2023-06-04 07:42:54 +0900

    インポート時に経度が反映されていないのを修正

--- cmd/root.go
+++ cmd/root.go
@@ -25,7 +25,7 @@ func Execute() {
 
 func init() {
 	rootCmd.CompletionOptions.HiddenDefaultCmd = true
-	rootCmd.Version="1.3.5"
+	rootCmd.Version="1.3.6"
 	rootCmd.PersistentFlags().StringP("port", "p", "auto", "Serial Port Name")
 	rootCmd.PersistentFlags().Bool("debug", false, "Show Debug Message")
 }
